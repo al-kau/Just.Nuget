ad2bc4b200275982162f3c54526aba08fcb1dbf9
alexander <al.kau+github@hotmail.com>
2023-06-17 16:53:28 +0300

    add library add test add nuget metadata

---
+++ source/Just.Nuget/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Alkau.Just.Nuget
+{
+    public static class Library
+    {
+        public static void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
