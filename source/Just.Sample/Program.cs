6e5043140d1318f68a1f00d97e76e0c1ee83c68f
alexander <116556921+al-kau@users.noreply.github.com>
2023-06-18 14:28:39 +0300

    add sample (#3)

---
+++ source/Just.Sample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Just.Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int a = 1;
+            const int b = -a;
+            var sum = Alkau.Just.Nuget.Library.Sum(a, b);
+            Alkau.Just.Nuget.Library.Log($"The sum of {a} and {b} is {sum}");
+        }
+    }
+}
