d9ab2fb5bb6401bd1a7515ddf27d127d4e7e8087
alexander <al.kau+github@hotmail.com>
2023-06-18 13:31:15 +0300

    fix library

--- source/Just.Nuget/Library.cs
+++ source/Just.Nuget/Library.cs
@@ -2,13 +2,26 @@
 
 namespace Alkau.Just.Nuget
 {
+    /// <summary>
+    /// This is just an example library.
+    /// </summary>
     public static class Library
     {
+        /// <summary>
+        /// It just prints text to the console.
+        /// </summary>
+        /// <param name="text">it's just some text</param>
         public static void Log(string text)
         {
             Console.WriteLine(text);
         }
 
+        /// <summary>
+        /// It's just a sum.
+        /// </summary>
+        /// <param name="a">The first term.</param>
+        /// <param name="b">The second term.</param>
+        /// <returns>It is the sum.</returns>
         public static int Sum(int a, int b)
         {
             return a + b;
